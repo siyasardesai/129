398211f20588a824a26963fe1cbe33250362fd3a
siyasardesai <71547814+siyasardesai@users.noreply.github.com>
2022-04-05 16:04:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -52,49 +52,7 @@ function gotPoses(results)
 function draw() {
 	image(video, 0, 0, 600, 500);
 
-	fill("#FF0000");
-	stroke("#FF0000");
-
-	if(scoreRightWrist > 0.2)
-	{ 
-		circle(rightWristX,rightWristY,20);
-
-		if(rightWristY >0 && rightWristY <= 100)
-		{
-			document.getElementById("speed").innerHTML = "Speed = 0.5x";		
-			song.rate(0.5);
-		}
-		else if(rightWristY >100 && rightWristY <= 200)
-		{
-			document.getElementById("speed").innerHTML = "Speed = 1x";		
-			song.rate(1);
-		}
-		else if(rightWristY >200 && rightWristY <= 300)
-		{
-			document.getElementById("speed").innerHTML = "Speed = 1.5x";		
-			song.rate(1.5);
-		}
-		else if(rightWristY >300 && rightWristY <= 400)
-		{
-			document.getElementById("speed").innerHTML = "Speed = 2x";		
-			song.rate(2);
-		}
-		else if(rightWristY >400)
-		{
-			document.getElementById("speed").innerHTML = "Speed = 2.5x";		
-			song.rate(2.5);
-		}
-	}
-
-	if(scoreLeftWrist > 0.2)
-	{
-		circle(leftWristX,leftWristY,20);
-		InNumberleftWristY = Number(leftWristY); 
-		remove_decimals = floor(InNumberleftWristY);
-		volume = remove_decimals/500;
-		document.getElementById("volume").innerHTML = "Volume = " + volume;		
-		song.setVolume(volume);	
-	}
+	
 
 }
 
